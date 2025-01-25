6134603f7e2068c32e3efc1cf84d914ecafc4360
Krzysztof Kraj <kraj.m.krzysztof@gmail.com>
2025-01-25 10:46:36 +0100

    Basic test for campaign added

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract CampaingFactory {
+contract CampaignFactory {
     address[] public deployedCampaigns;
 
     function createCampaign(uint minimum) public {
