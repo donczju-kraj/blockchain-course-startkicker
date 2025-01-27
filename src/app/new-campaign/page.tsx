6dd61f0572eaca03a91f26c9ee64b49fdcdf4d2c
Krzysztof Kraj <kraj.m.krzysztof@gmail.com>
2025-01-27 11:54:24 +0100

    Added next.js front-end

---
+++ src/app/new-campaign/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h2>Create new campaign!</h2>
+    </div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="border border-slate-200 bg-gray-900 px-4 py-2 flex flex-col items-center justify-center space-y-4">
+      <h1 className="text-xl font-semibold">Start kicker</h1>
+    </div>
+  );
+}
